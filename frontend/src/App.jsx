648044e7feb32b2b8592edc3a44156dba093830c
Karan Fartyal <karanfartyaloffice@gmail.com>
2025-09-12 12:57:21 +0530

    fixed error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -82,7 +82,7 @@ function App() {
   const { user } = useSelector((store) => store.auth);
   useEffect(() => {
     if (user) {
-      const socketio = io("http://localhost:3000", {
+      const socketio = io("https://social-media-app-bbfr.onrender.com", {
         query: {
           userId: user._id,
         },
