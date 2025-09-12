9890bab37a9d51a7b6e383281b3f992f48c7190a
Karan Fartyal <karanfartyaloffice@gmail.com>
2025-09-12 12:06:34 +0530

    fixed bug

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -99,7 +99,7 @@ export const login = async (req, res) => {
     return res
       .cookie("token", token, {
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 1 * 24 * 60 * 60 * 1000,
       })
       .json({
