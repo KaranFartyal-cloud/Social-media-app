fdaca7b896a26367311ef1fddeac17170c0cd1ee
karan fartyal <karanfartyaloffice@gmail.com>
2025-09-22 05:13:13 +0530

    backend url changes

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://frontend-bz9s.onrender.com",
   credentials: true,
 };
 
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://frontend-bz9s.onrender.com",
     methods: ["GET", "POST"],
   },
 });
